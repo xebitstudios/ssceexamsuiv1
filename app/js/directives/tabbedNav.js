7d00520b0a5c03dd063ccd2b8c5abbf295187112
TeeJay <xebitstudios@yahoo.com>
2017-03-04 18:27:07 -0500

    cut out

--- app/js/directives/tabbedNav.js
+++
@@ -1,8 +0,0 @@
-ssceexamsuiApp.directive('tabbedNav', function () {
-    return {
-        restrict: 'EA',
-        replace: true,
-        transclude: true,
-        templateUrl: 'templates/directives/MyTabsDirective.html'
-    };
-});
