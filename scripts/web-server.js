605cf08458b45fb2f8749ca49543cdc67b9bb0a4
TeeJay <xebitstudios@yahoo.com>
2017-05-27 13:20:09 -0400

    added once more

--- scripts/web-server.js
+++ scripts/web-server.js
@@ -22,9 +22,9 @@ var corsOptions = {
 //   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204 
 // }
 app.use(function(req, res, next) {
-	res.header('Access-Control-Allow-Origin', "*");
-	res.header('Access-Control-Allow-Methods', "GET,PUT,POST,DELETE");
-	res.header('Access-Control-Allow-Headers', "Content-Type");
+	res.setHeader('Access-Control-Allow-Origin', "*");
+	res.setHeader('Access-Control-Allow-Methods', "GET,PUT,POST,DELETE");
+	res.setHeader('Access-Control-Allow-Headers', "Origin, X-Requested-With, Content-Type, Accept");
 	next();
 });
 
