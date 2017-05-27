cc9e86b11f1caf086f44f465f8d96f9adf78c688
TeeJay <xebitstudios@yahoo.com>
2017-05-27 12:09:41 -0400

    added missing colon

--- scripts/web-server.js
+++ scripts/web-server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var path = require('path');
-var cors = require('cors')
+var cors = require('cors');
 var app = express();
 
 var bodyParser = require('body-parser');
